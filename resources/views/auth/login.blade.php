a906346152dfc009b38f00b6a6b953d0981b2a0c
Wendy <wendy.anugrah24@gmail.com>
2021-01-05 10:55:51 +0700

    logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,8 +16,10 @@ $showNav = false;
                         <form class="md-float-material form-material" method="POST" action="{{ route('login') }}">
                             @csrf
                             <div class="text-center">
-                                <img src="{{ asset('adminty\files\assets\images\logo.png') }}" alt="logo.png">
+                                <img src="{{ asset('adminty\files\assets\images\logo.png') }}" alt="logo.png" style="height: 100px">
                             </div>
+                            <h3 class="text-center" style="color:white">SDN 017
+                                Sungai Gantang</h3>
                             <div class="auth-box card">
                                 <div class="card-block">
                                     <div class="row m-b-20">
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -6,7 +6,9 @@
                 <i class="feather icon-menu"></i>
             </a>
             <a href="{{ route('dashboard') }}">
-                <img class="img-fluid" src="{{ asset('adminty\files\assets\images\logo.png') }}" alt="Theme-Logo">
+                <img class="img-fluid mt-10" src="{{ asset('adminty\files\assets\images\logo.png') }}" alt="Theme-Logo" style="height: 35px;">
+                <span class="text-center" style="font-size: 12px">SDN 017
+                Sungai Gantang</span>
             </a>
             <a class="mobile-options">
                 <i class="feather icon-more-horizontal"></i>
